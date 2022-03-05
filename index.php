7224f4d7e1634247a929ea9e0cbd41ea74d8c7b9
Fernando BK <fernandobenitezkleinert@gmail.com>
2022-03-05 16:11:37 -0300

    init

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-
-define('NOMBRE_BD', 'bd.sqlite');
-define('RUTA_DOC', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/doc.html');
-
-define('ID', $_GET['id']??'');
-define('LLAVE', str_replace('Bearer ', '', (apache_request_headers()['authorization']??'')));
-
 header('Allow: OPTIONS, HEAD, GET, POST');
 header('Content-type: application/json');
 header('Access-Control-Allow-Origin: '.($_SERVER['HTTP_ORIGIN']??'*'));
@@ -15,23 +8,27 @@ header('Access-Control-Allow-Headers: authorization,content-type');
 $_GET['id'] = trim(strtolower($_GET['id']));
 if( preg_match('/[^a-z0-9]/',$_GET['id']) ){ bad_request(); }
 
+$bd = new SQLite3('bd.sqlite');
+$time = time();
+$id = $_GET['id'] ?? '';
+$llave = str_replace('Bearer ', '', (apache_request_headers()['authorization']??''));
+
 function respuesta(int $status, string $msj = null, string $detalles = null){    
     $statusText = array();
     $statusText[200] = 'OK';
     $statusText[201] = 'Created';
     $statusText[202] = 'Accepted';
-    $statusText[205] = 'Reset Content';
     $statusText[400] = 'Bad Request';
+    $statusText[403] = 'Unauthorized';
     $statusText[404] = 'Not Found';
     $statusText[405] = 'Method Not Allowed';
-    $statusText[415] = 'Unsupported Media Type';
     $statusText[500] = 'Internal Server Error';
     
     if(!$detalles){
         $detalles = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'] ?? 'global';
         $detalles .= '-c' . $status;
     }
-    $detalles = RUTA_DOC.'#'.$detalles;
+    $detalles = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].'/doc.html#'.$detalles;
 
     header('HTTP/1.1 '.$status.' '.$statusText[$status]);
     header('X-Detalles: '.$detalles);
@@ -48,24 +45,26 @@ function respuesta(int $status, string $msj = null, string $detalles = null){
 
  switch($_SERVER['REQUEST_METHOD']){
     case 'OPTIONS':
-        header('HTTP/1.0 200 OK');
-        exit;
+        respuesta(200);
     break;
     
     case 'GET':
-        if( ID ){ mostrar_hoja(); }
-        else{ listar_todo(); }
+        if( ID ){ mostrar(); }
+        else{ listar_todo(); } 
     break;
 
     default:
         if(!ID){ respuesta(400, 'Falta ID'); }
 
         switch($_SERVER['REQUEST_METHOD']){
-            case 'POST': modificar_hoja(); break;
+            case 'POST': 
+                if( LLAVE ){ modificar_hoja(); }
+                else { crear_hoja(); }
+            break;
             
             case 'HEAD': 
                 if( LLAVE ){ borrar_hoja(); }
-                else { crear_hoja(); }
+                else { respuesta(403); }
             break;
             
             default: respuesta(405);
@@ -73,3 +72,73 @@ function respuesta(int $status, string $msj = null, string $detalles = null){
 }
 
 /* ==== Funciones propias del servicio ==== */
+
+function listar_todo(){
+    $bd = new SQLite3(NOMBRE_BD);
+    $result = $bd->query('SELECT * FROM tabla');
+    $items = array();
+    while( $item = $result->fetchArray(SQLITE3_ASSOC)){
+        unset($item['llave']);
+        $items[] = $item;
+    }
+    exit( json_encode( $items ) );
+}
+
+function mostrar(){
+
+    $bd = new SQLite3(NOMBRE_BD);
+    $sql = 'SELECT `hoja` FROM `carpeta` WHERE id = "'.ID.'"';
+    $result = $bd->query($sql);
+    $result = $result->fetchArray(SQLITE3_NUM);
+
+    if( $result ){ 
+        exit($result[0]); }
+    else{
+        respuesta(404, 'ID no encontrado.');
+    }
+}
+
+function crear(){
+    $id = TIME - 1646449200; // Fecha de primer commit
+    $data = addslashes(file_get_contents('php://input'));
+    $llave = md5((TIME/2).$id);
+
+    $bd = new SQLite3(NOMBRE_BD);
+    $sql = "INSERT INTO tabla (`id`, `data`, `llave`, `creado`) VALUES ($id, '$data', '$llave', datetime('now', 'localtime'))";
+    @$result = $bd->exec($sql);
+    
+    if( $result ){
+        respuesta(201, $llave);
+    }else{
+        respuesta(500, $id, '('.$bd->lastErrorCode().') '.$bd->lastErrorMsg() );
+    }
+}
+
+function modificar(){
+    $id = ID;
+    $llave = LLAVE;
+    $data = addslashes(file_get_contents('php://input'));
+
+    $bd = new SQLite3(NOMBRE_BD);
+    $sql = "UPDATE `tabla` SET `data` = '$data', `modificado` = datetime('now', 'localtime') WHERE `id` = '$id' AND  `llave` = '$llave';";
+    @$result = $bd->exec($sql);
+    
+    if( $result OR $bd->changes() ){
+        respuesta(202);
+    }else{
+        respuesta(500, '('.$bd->lastErrorCode().') '.$bd->lastErrorMsg() );
+    }
+}
+
+function borrar(){
+    $id = ID;
+    $bd = new SQLite3(NOMBRE_BD);
+    $sql = "DELETE FROM `tabla`  WHERE `id` = $id AND `llave` = '$llave';";
+    @$result = $bd->exec($sql);
+    
+    if( $result OR $bd->changes() ){
+        respuesta(202, $id);
+    }else{
+        respuesta(404, $id, '('.$bd->lastErrorCode().') '.$bd->lastErrorMsg() );
+    }
+}
